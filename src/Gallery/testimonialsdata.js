a806ae1750c1d1997d87229ca73f2094f630b418
progder <progderakash.gmail.com>
2022-11-25 20:12:20 +0530

    changes till 25 nov

---
+++ src/Gallery/testimonialsdata.js
@@ -0,0 +1,31 @@
+
+
+const data=[
+    {
+        img: "../images/usericon.jpg",
+        name: "name singh 1",
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa deleniti consectetur at iusto quod. Dolore blanditiis sunt velit aliquam labore!"
+
+    },
+     {
+        img: "../images/usericon.jpg",
+        name: "name singh 2",
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa deleniti consectetur at iusto quod. Dolore blanditiis sunt velit aliquam labore!"
+        
+    },
+     {
+        img: "../images/usericon.jpg",
+        name: "name singh 3",
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa deleniti consectetur at iusto quod. Dolore blanditiis sunt velit aliquam labore!"
+        
+    },
+     {
+        img: "../images/usericon.jpg",
+        name: "name singh 4",
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa deleniti consectetur at iusto quod. Dolore blanditiis sunt velit aliquam labore!"
+        
+    },
+
+
+]
+export default data;
