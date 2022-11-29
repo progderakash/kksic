2afa9d0d9038468f14d8fb062ad3237b70dcd7bb
progder <progderakash.gmail.com>
2022-11-29 16:25:25 +0530

    commit till 29 nov

---
+++ src/gototop/goTop.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './gotop.css'
+
+export const goTop = () => {
+  return (
+    <div className="gotop-container">
+        <a href="#home">top</a>
+    </div>
+  )
+}
